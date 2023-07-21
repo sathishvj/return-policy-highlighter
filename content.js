65b324eb0aa3d7b787f59eecc1bc7bbf95f66945
Sidharth P <sidharthpunathil714@gmail.com>
2023-07-21 16:34:35 +0530

    less clunky

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
+// icons: https://www.flaticon.com/authors/karyative/
+
 var getElementByXPath = function (xPath) {
     var xPathResult = document.evaluate(xPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     return xPathResult.singleNodeValue;
 };
 
-//console.log('Within Non-Returnable highlighter.');
-//
+
 
 var xpath='//*[@id="RETURNS_POLICY"]/span/div[2]/span';
 var xp = getElementByXPath(xpath);
@@ -14,16 +15,15 @@ if (!xp) {
 	xp = getElementByXPath(xpath);
 }
 
-const bar = document.createElement("div");
-//bar.style.backgroundColor = "yellow";
+const bar = document.createElement("li");
 bar.style.color = "red";
-bar.style.height = "30px";
-bar.style.width = "100%";
-bar.style.fontSize = "25px";
-bar.style.textAlign = "center";
+bar.style.fontSize = "14px";
+bar.style.fontWeight = "700";
+bar.style.fontFamily = "Amazon Ember,Arial,sans-serif";
+
 
 bar.innerHTML = xp.innerText;
 
-// Add bar on top of page.
-document.body.insertBefore(bar, document.body.firstChild);
+var targetElement = document.getElementsByClassName("a-unordered-list a-vertical a-spacing-mini")[0]
+targetElement.prepend(bar);
 
