9e009d7b7610f8cb3afcb9c4acbf35405c5b7275
sathishvj <sathishvj@yahoo.com>
2023-07-22 22:45:49 +0530

    added flipkart.com check, icons, info on loading extension manually in readme, error checks

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Why?
 Because I bought stuff and later realized that they were not returnable. One too many times. Fixing for that error.
 
 ![Screenshot](screenshot.png)
+
+# Adding the extension manually
+
+- First you have to download the code in this repo:
+  - If you have git, then: git clone https://github.com/sathishvj/return-policy-highlighter
+  - If you don't have git, then: find the "Code" button on the repo landing page, click the arrow next to it, then "Download ZIP", and the extract the zip file.
+- On Chrome browser, in the address bar enter chrome://extensions.
+- On the top left, there should be a button named "Load unpacked".
+- Choose the directory where this extension code (manifest.json and .js files) has been downloaded.
+
+# Why can't I put it on the Chrome Extensions webstore?
+
+To upload to the Chrome Extensions store, I have to register a developer account which requires a one time payment of USD5.
+Google isn't accepting my regular credit card with the error that it has been used with another currency. I don't have an alternate card or an alternative at the moment.
+
+# Updates
+
+- 2023/07/22 Added support for Flipkart.com
+- 2023/07/22 Added icons
+- 2023/07/22 Added error check if content returned is empty.
+- 2023/07/22 Added info to manually load extension.
--- content.js
+++ content.js
@@ -1,29 +1,62 @@
 var getElementByXPath = function (xPath) {
-    var xPathResult = document.evaluate(xPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    return xPathResult.singleNodeValue;
+  var xPathResult = document.evaluate(
+    xPath,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  );
+  return xPathResult.singleNodeValue;
 };
 
 //console.log('Within Non-Returnable highlighter.');
-//
 
-var xpath='//*[@id="RETURNS_POLICY"]/span/div[2]/span';
-var xp = getElementByXPath(xpath);
-// try others if above doesn't work
-if (!xp) {
-	xpath = '//*[@id="RETURNS_POLICY"]/div[2]/span';
-	xp = getElementByXPath(xpath);
+var returnPolicy = "";
+console.log("Within:", document.URL);
+if (document.URL.indexOf("amazon.") != -1) {
+  var xpath = '//*[@id="RETURNS_POLICY"]/span/div[2]/span';
+  var xp = getElementByXPath(xpath);
+  // try others if above doesn't work
+  if (!xp) {
+    xpath = '//*[@id="RETURNS_POLICY"]/div[2]/span';
+    xp = getElementByXPath(xpath);
+  }
+  if (xp) {
+    returnPolicy = xp.textContent;
+    if (returnPolicy.indexOf("Returns Policy") != -1) {
+      returnPolicy = "Info inside Returns Policy link on page.";
+    }
+  }
+} else if (document.URL.indexOf("flipkart.com") != -1) {
+  var xpath =
+    '//*[@id="container"]/div/div[3]/div[1]/div[2]/div[7]/div[1]/div/div[2]/div[2]/ul/li/div';
+  var xp = getElementByXPath(xpath);
+  // try others if above doesn't work
+  if (!xp) {
+    var xpath =
+      '//*[@id="container"]/div/div[3]/div[1]/div[2]/div[6]/div[1]/div/div[2]/div[2]/ul/li/div';
+    xp = getElementByXPath(xpath);
+  }
+  if (xp) {
+    returnPolicy = xp.textContent;
+    if (returnPolicy.endsWith("?")) {
+      returnPolicy = returnPolicy.slice(0, -1);
+    }
+  }
 }
 
-const bar = document.createElement("div");
-//bar.style.backgroundColor = "yellow";
-bar.style.color = "red";
-bar.style.height = "30px";
-bar.style.width = "100%";
-bar.style.fontSize = "25px";
-bar.style.textAlign = "center";
+if (returnPolicy != "") {
+  const bar = document.createElement("div");
+  //bar.style.backgroundColor = "yellow";
+  bar.style.color = "red";
+  bar.style.height = "30px";
+  bar.style.width = "100%";
+  bar.style.fontSize = "25px";
+  bar.style.textAlign = "center";
 
-bar.innerHTML = xp.innerText;
+  //bar.innerHTML = xp.innerText;
+  bar.innerHTML = returnPolicy;
 
-// Add bar on top of page.
-document.body.insertBefore(bar, document.body.firstChild);
-
+  // Add bar on top of page.
+  document.body.insertBefore(bar, document.body.firstChild);
+}
